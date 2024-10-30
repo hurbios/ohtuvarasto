4b61b897389cc1cba687970491a19f94ea80f12d
hurbios <15719468+hurbios@users.noreply.github.com>
2024-10-30 19:03:07 +0200

    add coverage and update varasto tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -18,6 +18,9 @@ class TestVarasto(unittest.TestCase):
 
         self.assertAlmostEqual(self.varasto.saldo, 8)
 
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 8)
+
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
 
@@ -38,3 +41,33 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+        
+
+    def test_init_varasto_tilavuudella_alle_0(self):
+        uusi_varasto = Varasto(-1.0, -1.0)
+        self.assertAlmostEqual(uusi_varasto.tilavuus, 0)
+        self.assertAlmostEqual(uusi_varasto.saldo, 0)
+ 
+    def test_init_varasto_saldo_suurempi_kuin_tilavuus(self):
+        uusi_varasto = Varasto(1.0, 2.0)
+        self.assertAlmostEqual(uusi_varasto.tilavuus, 1)
+        self.assertAlmostEqual(uusi_varasto.saldo, 1)
+ 
+    def test_lisaa_varastoon_enemman_kuin_mahtuu(self):
+        uusi_varasto = Varasto(2.0, 1.0)
+
+        uusi_varasto.lisaa_varastoon(uusi_varasto.paljonko_mahtuu()+1)
+        self.assertAlmostEqual(uusi_varasto.saldo, uusi_varasto.tilavuus)
+        
+    def test_ota_varastosta_vaarin(self):
+        alku_saldo = 1.0
+        uusi_varasto = Varasto(2.0, alku_saldo)
+
+        uusi_varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(uusi_varasto.saldo, alku_saldo)
+        uusi_varasto.ota_varastosta(20)
+        self.assertAlmostEqual(uusi_varasto.saldo, 0)
+
+    def test_tostring(self):
+        uusi_varasto = Varasto(1.0, 1.0)
+        self.assertEqual(str(uusi_varasto), 'saldo = 1.0, vielä tilaa 0.0')
